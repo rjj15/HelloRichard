7983fc973c5609f2be064c7b591b3af3165dc418
rjj15 <rjj15@yahoo.com>
2015-10-06 11:57:12 -0800

    Stock Ticker app

---
+++ HelloRichard/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace HelloRichard
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.TB1 = new System.Windows.Forms.TextBox();
+            this.LB1 = new System.Windows.Forms.Label();
+            this.RT1 = new System.Windows.Forms.RichTextBox();
+            this.TB2 = new System.Windows.Forms.TextBox();
+            this.nullpop = new System.Windows.Forms.ErrorProvider(this.components);
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.nullpop)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(132, 454);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(130, 74);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Get Quote";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // TB1
+            // 
+            this.TB1.Location = new System.Drawing.Point(394, 22);
+            this.TB1.Name = "TB1";
+            this.TB1.Size = new System.Drawing.Size(764, 26);
+            this.TB1.TabIndex = 1;
+            this.TB1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // LB1
+            // 
+            this.LB1.AutoSize = true;
+            this.LB1.Location = new System.Drawing.Point(250, 28);
+            this.LB1.Name = "LB1";
+            this.LB1.Size = new System.Drawing.Size(100, 20);
+            this.LB1.TabIndex = 2;
+            this.LB1.Text = "Enter Quote:";
+            this.LB1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // RT1
+            // 
+            this.RT1.Location = new System.Drawing.Point(394, 116);
+            this.RT1.Name = "RT1";
+            this.RT1.Size = new System.Drawing.Size(764, 332);
+            this.RT1.TabIndex = 3;
+            this.RT1.Text = "";
+            // 
+            // TB2
+            // 
+            this.TB2.Location = new System.Drawing.Point(394, 71);
+            this.TB2.Name = "TB2";
+            this.TB2.Size = new System.Drawing.Size(764, 26);
+            this.TB2.TabIndex = 4;
+            // 
+            // nullpop
+            // 
+            this.nullpop.ContainerControl = this;
+            this.nullpop.RightToLeftChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button2
+            // 
+            this.button2.ForeColor = System.Drawing.Color.DarkRed;
+            this.button2.Location = new System.Drawing.Point(296, 454);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(165, 74);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Quit";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1268, 540);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.TB2);
+            this.Controls.Add(this.RT1);
+            this.Controls.Add(this.LB1);
+            this.Controls.Add(this.TB1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.nullpop)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox TB1;
+        private System.Windows.Forms.Label LB1;
+        private System.Windows.Forms.TextBox TB2;
+        public System.Windows.Forms.RichTextBox RT1;
+        private System.Windows.Forms.ErrorProvider nullpop;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ HelloRichard/Form1.cs
@@ -0,0 +1,112 @@
+﻿//Richard Johnson.  This program takes a stock ticker input, puts together a http string, and queries yahoo for stock price
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+
+namespace HelloRichard
+{
+ 
+    public partial class Form1 : Form
+    {
+        public int up1 = 0;
+        public string ticker1="";
+        public string yahoo = "http://download.finance.yahoo.com/d/quotes.csv?s=";
+        public string endyahoo = "&f=sl1d1t1c1ohgv&e=.csv";
+    //lists are not being used yet
+        public List<string> data = new List<string>();
+        public List<string>titles = new List<string>();
+        public List<string>row = new List<string>();
+        List<string> Stockcollection = new List<string>();
+        public Form1()
+        {
+            InitializeComponent();
+            titles.Add("Ticker");
+            titles.Add("Price");
+            titles.Add("Date");
+            titles.Add("Time");
+            titles.Add("Change");
+            titles.Add("Open");
+            titles.Add("Day high");
+            titles.Add("Day Low");
+            titles.Add("Volume");
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            ticker1 = TB1.Text;
+            TB2.Text = yahoo + ticker1 +endyahoo;
+        }
+        private void label1_Click(object sender, EventArgs e)
+        {
+           
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ++up1;
+            LB1.Text = "Count is: " + up1;
+            if (TB1 != null)
+            {
+                //send value of TB1 to Output for debugging
+                System.Diagnostics.Trace.WriteLine("TB1" + TB1);
+                var url = TB2.Text;
+                //string url = TB2.text;
+                RT1.Text += DownloadString(url);
+                string text = RT1.Text;
+                char[] separatingChars = { '"', '"', ',', ',' };
+                string[] words = text.Split(separatingChars, System.StringSplitOptions.RemoveEmptyEntries);
+                System.Console.WriteLine("words4 is:" +words[4]);
+                if (words[4].Contains("-"))
+                {
+                    //if stock is negative change background to Red
+                    RT1.BackColor = System.Drawing.Color.Red;
+                    words[4] = null;
+                    System.Console.WriteLine("after setting words4 to null it is" + words[4]);
+                    text = null;
+                }
+                else
+                {
+                    RT1.BackColor = System.Drawing.Color.Yellow;
+                    words[4] = null;
+                    text = null;
+                }
+                //foreach (string s in words)
+                // {
+
+                // System.Console.WriteLine(words(s));
+                // }
+
+
+            }
+
+            else
+            {
+                MessageBox.Show("empty Ticker!");
+
+            }
+        
+        }
+        public static String DownloadString(string address)
+        {
+            WebClient client = new WebClient();
+            string reply = client.DownloadString(address);
+            //RT1.AppendText(String.Format(reply));
+            return reply;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Quit Really!!");
+            Application.Exit();
+        }
+    }
+}
